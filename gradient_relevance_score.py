a0018cf6a471428a74c6eda4c43dedc5219d2a38
elias <elias.strauss@campus.tu-berlin.de>
2025-06-17 13:09:12 +0200

    natural language explanations using openai api

---
+++ gradient_relevance_score.py
@@ -0,0 +1,55 @@
+import torch
+from transformers import DistilBertForSequenceClassification, DistilBertTokenizer
+from captum.attr import LayerIntegratedGradients
+
+class DistilBertAttributor:
+    def __init__(self, model_path: str, force_cpu: bool = False, max_length: int = 128):
+        self.tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+        self.model = DistilBertForSequenceClassification.from_pretrained(model_path)
+        self.model.eval()
+
+        self.device = torch.device("mps" if torch.backends.mps.is_available() and not force_cpu else "cpu")
+        self.model.to(self.device)
+
+        self.max_length = max_length
+        self.lig = LayerIntegratedGradients(self._forward_captum, self.model.distilbert.embeddings)
+
+    def _forward_captum(self, input_ids, attention_mask):
+        return self.model(input_ids=input_ids, attention_mask=attention_mask).logits
+
+    def compute_attributions(self, text: str, merge_scores: bool = True, p=2):
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, padding='max_length', max_length=self.max_length)
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        baseline = torch.zeros_like(input_ids).to(self.device)
+
+        with torch.no_grad():
+            logits = self.model(**inputs).logits
+            target = torch.argmax(logits, dim=1).item()
+
+        attributions, delta = self.lig.attribute(
+            inputs=input_ids,
+            baselines=baseline,
+            additional_forward_args=attention_mask,
+            target=target,
+            return_convergence_delta=True
+        )
+
+        scores = attributions.norm(p=p, dim=-1).squeeze(0)
+        tokens = self.tokenizer.convert_ids_to_tokens(input_ids[0])
+
+        filter_set = {"[CLS]", "[SEP]", "[PAD]", '.', ',', "(", ")"}
+        token_scores =  {} if merge_scores else \
+                        {token: score for token, score in zip(tokens, scores)
+                            if token not in filter_set}
+
+        if merge_scores:
+            for token, score in zip(tokens, scores):
+                if token not in filter_set:
+                    if token in token_scores:
+                        token_scores[token] += score
+                    else:
+                        token_scores[token] = score
+
+        return target, sorted(token_scores.items(), key=lambda x: x[1], reverse=True)
---
+++ natural_language_explanation.py
@@ -0,0 +1,3 @@
+from gradient_relevance_score import DistilBertAttributor
+
+attributor = DistilBertAttributor(model_path="./results/distilbert/checkpoint-170")
